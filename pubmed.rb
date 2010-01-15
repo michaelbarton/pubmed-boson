15493517c84593e0ab0213ffe82c72d03cff19a4
Michael Barton <mail@michaelbarton.me.uk>
2010-01-15 18:27:30 -0500

    Pubmed query command for boson

---
+++ pubmed.rb
@@ -0,0 +1,26 @@
+require 'bio'
+
+module PubMed
+
+  # @render_options :fields => {
+  #   :default => [:PMID, :first_author, :year, :journal, :title]}
+  # @options :email => :string, :number => 5, :year => :numeric,
+  #  :authors => :array, :journal => :string
+  def query(terms,options={})
+    query = String.new
+    query << " #{options[:year]} [dp]" if options[:year]
+    query << " #{options[:journal]} [ta]" if options[:journal]
+    options[:authors].each{|a| query << " #{a} [au]"} if options[:authors]
+
+    results = Bio::PubMed.esearch(query << terms,"email" => options[:email])
+    results = results[0..(options[:number]-1)]
+    results.map do |result|
+      m = Bio::MEDLINE.new(Bio::PubMed.query(result))
+      {:title        => m.title,         :year   => m.year,
+       :first_author => m.authors.first, :PMID   => m.pmid,
+       :journal      => m.journal
+      }
+    end
+  end
+
+end
